50d03a096b76805358eb6641c455def4569d9aa7
John <johnnam31@gmail.com>
2015-06-04 13:16:57 +0900

    추천 디자인 수정.

--- WebContent/js/preferenceController.js
+++ WebContent/js/preferenceController.js
@@ -17,7 +17,7 @@ $(function(){
  				
  				$('#preference_wrapper .write-rec-bar').append("We think you like "+postingPreferenceDatas[0].tasteR+postingPreferenceDatas[0].typeR+postingPreferenceDatas[0].timeR+"<br>");
  				if(postingPreferenceDatas.length==1){
- 	 				$('.preference-posts').append("We are sorry but we couldn't find restaurent that you might like");
+ 	 				$('#preference_notFound').append("We are sorry but we couldn't find restaurent that you might like");
  				}
  				for(var i=1; i<postingPreferenceDatas.length; i++ ){
 					if(window.sessionStorage.getItem('id')==postingPreferenceDatas[i].writer){
