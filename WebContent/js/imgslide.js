b746a8ee19124da84b6c1f5a26d4944b49e0673f
John <johnnam31@gmail.com>
2015-06-09 01:11:20 +0900

    히히.

--- WebContent/js/imgslide.js
+++ WebContent/js/imgslide.js
@@ -1,22 +1,13 @@
 var additionalPosting;
 
 $(function() {
-	$(document).on('click','.more-content',function(){
+	$(document).on('click', '.more-content', function(){
 		
 		var posting_seq=$(this).closest('section').attr('id');
 		posting_seq=posting_seq.substring(8);
 		
 		var imgDatas;
 		var slidecontent;
-		var slideArrow=
-				'<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">'+
-	    			'<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>'+
-	    			'<span class="sr-only">Previous</span>'+
-	    		'</a>'+
-	    		'<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">'+
-	    			'<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>'+
-	    			'<span class="sr-only">Next</span>'+
-	    		'</a>'
 		
 		$.ajax({	//Request Login API
 			url: '/getImg?posting_seq='+posting_seq,
@@ -54,7 +45,7 @@ $(function() {
 		    			'<span class="sr-only">Next</span>'+
 		    		'</a>'+
 							'</div>'
-					$('#img-slide').append(slidecontent+slideArrow);
+					$('#img-slide').append(slidecontent);
 				}else if(imgDatas.length==3){
 					slidecontent=
 						'<div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="width:350px;">'+					
@@ -82,42 +73,7 @@ $(function() {
 				}
 			}
 		});
-<<<<<<< HEAD
-			
-		if(imgDatas[0]=='no-image.jpg'){
-			$('#moreContentsModal').modal('hide');
-			console.log('hiding');
-		}
-		else if(imgDatas.length==1){
-			slidecontent=
-				'<img data-slidr="one" class="slideImg" src="img/'+imgDatas[0]+'"/>'
-			$('#carousel-example-generic').append(slidecontent);
-		}else if(imgDatas.length==2){
-			slidecontent=
-				'<div class="carousel-inner" role="listbox">'+
-					'<div class="item active">'+
-						'<img class="slideImg" src="img/'+imgDatas[0]+'"/>'+
-					'</div>'+
-					'<div class="item">'+
-						'<img class="slideImg" src="img/'+imgDatas[1]+'"/>'+
-					'</div>'+
-				'</div>'
-			$('#carousel-example-generic').append(slidecontent+slideArrow);
-		}else if(imgDatas.length==3){
-			slidecontent=
-				'<div class="carousel-inner" role="listbox">'+
-					'<div class="item active">'+
-						'<img class="slideImg" src="img/'+imgDatas[0]+'">'+
-					'</div>'+
-					'<div class="item">'+
-						'<img class="slideImg" src="img/'+imgDatas[1]+'">'+
-					'</div>'+
-					'<div class="item">'+
-						'<img class="slideImg" src="img/'+imgDatas[2]+'">'+
-					'</div>'+
-				'</div>'
-			$('#carousel-example-generic').append(slidecontent+slideArrow);
-		}
+
 		$.ajax({
 			url : '/getPosting?type=5&posting_seq='+posting_seq,
 			method : 'GET',
@@ -135,24 +91,21 @@ $(function() {
 				$('#moreContents').append(getModalItem(additionalPosting[0]));
 			}
 		});
-=======
->>>>>>> f53148aea8e042877f334dde599095174f70da7e
-	})
+
 	
 	//슬라이드 모달 초기화 
 	$('#moreContentsModal').on('hidden.bs.modal', function (e) {
-<<<<<<< HEAD
 		$('#carousel-example-generic').empty();
 		additionalPosting = null;
 		console.log('empting');
-		
-=======
+
 		$('#img-slide').empty();
 		console.log('empting')
->>>>>>> f53148aea8e042877f334dde599095174f70da7e
+
 	});
 
 });
+})
 
 //내용 연
 function getModalItem(additionalPosting){
